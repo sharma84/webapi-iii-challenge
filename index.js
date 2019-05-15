f746dcaaedfbbd8806c8f5816c33effb37420ed2
Snigdha Regmi <sregmi@NORD.LOCAL>
2019-05-14 22:53:37 -0600

    mvp almost done, need to check /:id/posts

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+//yarn add express
+//import server and require is to import express module
+const server = require("./server.js");
+
+server.listen(9090, () => {
+  console.log("listening on port 9090");
+});
--- server.js
+++ server.js
@@ -1,15 +1,39 @@
-const express = 'express';
+const express = require("express");
+//const logger = require('morgan');
 
-const server = express();
+const server = express(); //call express to get server
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+//routers
+const postRouter = require("./posts/postRouter.js");
+const userRouter = require("./users/userRouter.js");
+
+//global middleware
+
+server.use(express.json());
+server.use(logger);
+//server.use(helmet());
+server.use("/api/posts", postRouter); //refer to const postRouter
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
+  console.log(
+    `${req.method}:METHOD, ${req.url}:URL, ${new Date().toISOString()}:DATE`
+  ); //get Request, put request
+  next();
+}
 
-};
+//global error handler
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    message: "error",
+    err
+  });
+});
 
 module.exports = server;
